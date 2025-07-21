eb41d917f1188526a757740f96460967bc4cceda
s1f10250266 <s1f102502665@iniad.org>
2025-07-21 15:20:27 +0900

    addtion

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import re
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 kinoko_count = 3
 takenoko_count = 5
 messages = ['Kinoko is wonrderful!', 'Takenoko is awesome!']
 
-@app.route('/')
+@app.route('/api/v1/votes')
+def vote_api():
+    return jsonify({'kinoko':kinoko_count, 'takenoko':takenoko_count})
 def top():
     return render_template('index.html', **vars())
 
